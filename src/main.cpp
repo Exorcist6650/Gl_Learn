9ce53b5f14179f67c7254928c99a172e9c8e105a
ExaPC <Exorcist6650@gmail.com>
2026-01-01 22:32:37 +1000

    First triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,173 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+std::string getProjectPath(){
+	std::string path = __FILE__;
+	path = path.substr(0, path.find_last_of("\\/")); // GL_learn/src
+	return path.substr(0, path.find_last_of("\\/")); 
+}
+
+GLfloat vertex[]
+{
+	// positions        // colors
+	0.0f, 0.5f, 0.0f,   0.0f, 0.0f, 1.0f, // top
+	-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
+	0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f  // bottom right
+};
+
 int main(void)
 {
-    GLFWwindow* window;
+	GLFWwindow* ptrWindow;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	// Initialize the library
+	if (!glfwInit())
+	{
+		std::cout << "ERROR::GLFW::INIT_FAILED" << std::endl;
+		return -1;
+	}
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	// Opengl version setting
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	/* Create a windowed mode window and its OpenGL context */
+	ptrWindow = glfwCreateWindow(640, 480, "Triangle", NULL, NULL);
+	if (!ptrWindow)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-                                        
-    glClearColor(1, 1, 1, -1);
+	/* Make the window's context current */
+	glfwMakeContextCurrent(ptrWindow);
+	 
+	if (!gladLoadGL()) std::cout << "ERROR::GLAD::OPEN_FAILED" << std::endl; // Glad exception
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+	// Device info
+	std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+	std::cout  << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+	// Vertex array
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
 
-    glfwTerminate();
-    return 0;
+	// Vertex buffer
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
+	// Position
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+	glEnableVertexAttribArray(0);
+	// Color
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+
+	// Shaders read
+	std::string vertexCode;
+	std::string fragmentCode;
+	std::ifstream vShaderFile;
+	std::ifstream fShaderFile;
+	// Exceptions
+	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	try
+	{
+		// Open files
+		vShaderFile.open(getProjectPath() + "/res/shaders/shader.vert");
+		fShaderFile.open(getProjectPath() + "/res/shaders/shader.frag");
+		if (!vShaderFile.is_open()) std::cout << "ERROR::SHADER::VERTEX::FILE_NOT_OPEN" << std::endl;
+		if (!fShaderFile.is_open()) std::cout << "ERROR::SHADER::FRAGMENT::FILE_NOT_OPEN" << std::endl;
+
+		std::stringstream vShaderStream, fShaderStream; // String stream
+
+		// Read files buffers into string streams
+		vShaderStream << vShaderFile.rdbuf(); // ifstream::rdbuf() transfer all file (not getline reading)
+		fShaderStream << fShaderFile.rdbuf();
+
+		// Closing file handlers
+		vShaderFile.close();
+		fShaderFile.close();
+
+		// Stream to string convertation
+		vertexCode = vShaderStream.str(); // From thread to string
+		fragmentCode = fShaderStream.str();
+	}
+	catch (std::ifstream::failure ex)
+	{
+		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+	}
+	const char* vShaderCode = vertexCode.c_str(); // C str convertation for OpenGL
+	const char* fShaderCode = fragmentCode.c_str();
+
+	char infoLog[512]; // For exceptions
+	GLint success;
+
+	// Vertex shader compiling
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vShaderCode, NULL);
+	glCompileShader(vertexShader);
+	// Vertex compiling errors
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Fragment shader compiling
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fShaderCode, NULL);
+	glCompileShader(fragmentShader);
+	// Fragment compiling errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPLATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Shader program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Program linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	// Shader deleting
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(ptrWindow))
+	{
+		/* Render here */
+		glClearColor(1.0f, 1.0f, 1.0f, -1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+		glUseProgram(shaderProgram);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(ptrWindow); // Swap front and back buffers 
+		glfwPollEvents(); // Events checking
+	}
+
+	glfwTerminate();
+	return 0;
 }
